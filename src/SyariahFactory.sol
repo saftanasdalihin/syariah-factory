5451141065a8fa78f7b38601a16c1cde6cbd5fcf
Safta Nasdalihin <saftanasdalihin@gmail.com>
2026-04-13 12:48:34 +0700

    update: add mudharabah, murabahah, and wadiah contract blueprint

---
+++ src/SyariahFactory.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./Mudharabah.sol";
+import "./Murabahah.sol";
+import "./Wadiah.sol";
+
+contract SyariahFactory {
+    Mudharabah public mudharabah;
+    Murabahah public murabahah;
+    Wadiah public wadiah;
+
+    constructor() {
+        mudharabah = new Mudharabah();
+        murabahah = new Murabahah();
+        wadiah = new Wadiah();
+    }
+}
