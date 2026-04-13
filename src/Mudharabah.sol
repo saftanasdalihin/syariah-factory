99bd30cbe32be61b0ee0db10ec14f8d65aab3770
Safta Nasdalihin <saftanasdalihin@gmail.com>
2026-04-14 06:20:31 +0700

    update: build blueprint for Mudharabah, Murabahah, and wadiah

--- src/Mudharabah.sol
+++ src/Mudharabah.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract Mudharabah {}
+contract Mudharabah {
+    uint256 public capital;
+    uint256 public profitShare;
+
+    constructor(uint256 _capital, uint256 _profitShare) {
+        capital = _capital;
+        profitShare = _profitShare;
+    }
+}
--- src/Murabahah.sol
+++ src/Murabahah.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract Murabahah {}
+contract Murabahah {
+    uint256 public cost;
+    uint256 public profit;
+
+    constructor(uint256 _cost, uint256 _profit) {
+        cost = _cost;
+        profit = _profit;
+    }
+}
